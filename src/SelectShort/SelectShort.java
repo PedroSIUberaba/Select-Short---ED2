f242ad1e13033cdb8769eb2d88d3e38e53f2d8a4
Pedro <pedhenriquecm@gmail.com>
2025-03-01 15:47:42 -0300

    first commit

---
+++ src/SelectShort/SelectShort.java
@@ -0,0 +1,37 @@
+package SelectShort;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class SelectShort {
+
+    public static void main(String[] args) {
+        //int[] vetor = {3, 5, 6, 8, 9};
+        int nt = 10;
+        int[] vetor = new int[nt];
+        Random rand = new Random();
+
+        for(int i = 0; i < nt; i++){
+            vetor[i] = rand.nextInt(101)*10;
+        }
+
+        int tv = vetor.length;
+
+        for(int i = 0; i < tv-1; i++){
+            int vmenor = vetor[i];
+            int pmenor = i;
+            for(int j = i + 1; j < tv; j++){
+                if (vmenor > vetor[j]){
+                    vmenor = vetor[j];
+                    pmenor = j;
+                }
+            }
+            int bkp = vetor[i];
+            vetor[i] = vmenor;
+            vetor[pmenor] = bkp;
+        }
+        System.out.println(Arrays.toString(vetor));
+    }
+
+
+}
